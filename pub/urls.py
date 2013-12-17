f866adb6c06b9f25a080b9530f25805e6fa750b4
eXthen <exthen@gmail.com>
2013-12-17 17:41:48 +0800

    用户酒吧管理

--- pub/urls.py
+++ pub/urls.py
@@ -18,9 +18,11 @@ app.add_url_rule('/logout', 'logout_view', logout_view, methods=('GET', 'POST'))
 # 后台管理路径
 from .views.admin_user import SuperUserView, ManagerUserView, UserView
 from .views.admin_pub import PubView
+from .views.admin_pub import SinglePubView
 admin.add_view(SuperUserView(db, name=u'管理员', endpoint='superuser', category=u"猫吧客"))
 admin.add_view(ManagerUserView(db, name=u'管理员', endpoint='manageruser', category=u"酒吧客户"))
-admin.add_view(PubView(db, name=u'酒吧管理'))
+admin.add_view(PubView(db, name=u'酒吧管理', endpoint='pubview'))
+admin.add_view(SinglePubView(db, name=u'酒吧管理', endpoint='singlepubview'))
 admin.add_view(UserView(db, name=u'会员管理'))
 
 # 微信接口处理路径
--- pub/views/admin_pub.py
+++ pub/views/admin_pub.py
@@ -13,6 +13,9 @@ from flask.ext.admin.form import get_form_opts
 from wtforms.fields import TextField
 from wtforms import validators
 from flask.ext import login
+from sqlalchemy import or_
+from sqlalchemy.orm import joinedload
+from flask.ext.admin.contrib.sqla import tools
 
 from ..models.pub import Pub
 from ..models.admin_user import AdminUser
@@ -227,3 +230,279 @@ class PubView(ModelView):
 
     def is_accessible(self):
         return login.current_user.is_normal_superuser()
+
+
+class SinglePubView(PubView):
+    """酒吧管理员管理管理自己酒吧的视图"""
+
+    can_create = False
+    can_delete = False
+    column_display_pk = True
+
+    column_exclude_list = ('intro', 'access_token', 'access_token_time', 'secret', 'address', 'tel',
+                           'create_time', 'base_path', 'rel_path', 'pic_name', 'logo', 'token', 'appid')
+
+    def __init__(self, db, **kwargs):
+        ModelView.__init__(self, Pub, db, **kwargs)
+
+    def scaffold_form(self):
+        """改写form"""
+        form_class = super(PubView, self).scaffold_form()
+        delete_attrs(form_class, ('access_token_time', 'access_token', 'token', 'appid', 'secret', 'create_time',
+                                  'stop_time', 'status', 'base_path', 'rel_path', 'pic_name', 'logo'))
+        form_class.user = TextField(label=u'酒吧管理员', validators=[validators.required(), validators.length(max=16)])
+        form_class.password = TextField(label=u'管理员密码', validators=[validators.required()])
+
+        return form_class
+
+    def is_accessible(self):
+        return login.current_user.is_normal_manageruser()
+
+    def update_model(self, form, model):
+        """更新酒吧和酒吧管理员"""
+        try:
+            form_dict = form_to_dict(form)
+
+            pub = Pub.query.filter(Pub.id == model.id).first()
+            self._update_pub(pub, form_dict)
+            user = self._get_pub_admin(model.id)
+            if user is None:
+                user = self._get_user(form_dict, pub.id)
+                self.session.add(user)
+            else:
+                if not self._update_user(user, form_dict):
+                    return False
+
+            self._on_model_change(form, model, False)
+            self.session.commit()
+            self.after_update_model(model.id)  # 创建菜单，更新数据库资料
+        except Exception as ex:
+            if self._debug:
+                raise
+
+            flash(gettext('Failed to update model. %(error)s', error=str(ex)), 'error')
+            log.exception('Failed to update model')
+            self.session.rollback()
+
+            return False
+        else:
+            self.after_model_change(form, model, False)
+
+        return True
+
+    def _update_pub(self, pub, form_dict):
+        pub.update(name=form_dict.get('name'),
+                   intro=form_dict.get('intro', None),
+                   appid=form_dict.get('address'),
+                   secret=form_dict.get('tel'))
+
+    def get_list(self, page, sort_column, sort_desc, search, filters, execute=True, pub_id=None):
+        """
+            Return models from the database.
+
+            :param page:
+                Page number
+            :param sort_column:
+                Sort column name
+            :param sort_desc:
+                Descending or ascending sort
+            :param search:
+                Search query
+            :param execute:
+                Execute query immediately? Default is `True`
+            :param filters:
+                List of filter tuples
+        """
+
+        # Will contain names of joined tables to avoid duplicate joins
+        joins = set()
+
+        query = self.get_query()
+        count_query = self.get_count_query()
+
+        if pub_id is None:
+            flash("系统错误，没有查询到pub_id", 'error')
+            raise ValueError
+        else:
+            query = query.filter(Pub.id == pub_id)
+
+        # Apply search criteria
+        if self._search_supported and search:
+            # Apply search-related joins
+            if self._search_joins:
+                for jn in self._search_joins.values():
+                    query = query.join(jn)
+                    count_query = count_query.join(jn)
+
+                joins = set(self._search_joins.keys())
+
+            # Apply terms
+            terms = search.split(' ')
+
+            for term in terms:
+                if not term:
+                    continue
+
+                stmt = tools.parse_like_term(term)
+                filter_stmt = [c.ilike(stmt) for c in self._search_fields]
+                query = query.filter(or_(*filter_stmt))
+                count_query = count_query.filter(or_(*filter_stmt))
+
+        # Apply filters
+        if filters and self._filters:
+            for idx, value in filters:
+                flt = self._filters[idx]
+
+                # Figure out joins
+                tbl = flt.column.table.name
+
+                join_tables = self._filter_joins.get(tbl, [])
+
+                for table in join_tables:
+                    if table.name not in joins:
+                        query = query.join(table)
+                        count_query = count_query.join(table)
+                        joins.add(table.name)
+
+                # Apply filter
+                query = flt.apply(query, value)
+                count_query = flt.apply(count_query, value)
+
+        # Calculate number of rows
+        count = count_query.scalar()
+
+        # Auto join
+        for j in self._auto_joins:
+            query = query.options(joinedload(j))
+
+        # Sorting
+        if sort_column is not None:
+            if sort_column in self._sortable_columns:
+                sort_field = self._sortable_columns[sort_column]
+
+                query, joins = self._order_by(query, joins, sort_field, sort_desc)
+        else:
+            order = self._get_default_order()
+
+            if order:
+                query, joins = self._order_by(query, joins, order[0], order[1])
+
+        # Pagination
+        if page is not None:
+            query = query.offset(page * self.page_size)
+
+        query = query.limit(self.page_size)
+
+        # Execute if needed
+        if execute:
+            query = query.all()
+
+        return count, query
+
+    # Views
+    @expose('/')
+    def index_view(self):
+        """
+            List view
+        """
+        # Grab parameters from URL
+        page, sort_idx, sort_desc, search, filters = self._get_extra_args()
+
+        # Map column index to column name
+        sort_column = self._get_column_by_idx(sort_idx)
+        if sort_column is not None:
+            sort_column = sort_column[0]
+
+        pub_id = self.get_pub_id()
+
+        # Get count and data
+        count, data = self.get_list(page, sort_column, sort_desc,
+                                    search, filters, pub_id=pub_id)
+
+        # Calculate number of pages
+        num_pages = count // self.page_size
+        if count % self.page_size != 0:
+            num_pages += 1
+
+        # Pregenerate filters
+        if self._filters:
+            filters_data = dict()
+
+            for idx, f in enumerate(self._filters):
+                flt_data = f.get_options(self)
+
+                if flt_data:
+                    filters_data[idx] = flt_data
+        else:
+            filters_data = None
+
+        # Various URL generation helpers
+        def pager_url(p):
+            # Do not add page number if it is first page
+            if p == 0:
+                p = None
+
+            return self._get_url('.index_view', p, sort_idx, sort_desc,
+                                 search, filters)
+
+        def sort_url(column, invert=False):
+            desc = None
+
+            if invert and not sort_desc:
+                desc = 1
+
+            return self._get_url('.index_view', page, column, desc,
+                                 search, filters)
+
+        # Actions
+        actions, actions_confirmation = self.get_actions_list()
+
+        return self.render(self.list_template,
+                               data=data,
+                               # List
+                               list_columns=self._list_columns,
+                               sortable_columns=self._sortable_columns,
+                               # Stuff
+                               enumerate=enumerate,
+                               get_pk_value=self.get_pk_value,
+                               get_value=self.get_list_value,
+                               return_url=self._get_url('.index_view',
+                                                        page,
+                                                        sort_idx,
+                                                        sort_desc,
+                                                        search,
+                                                        filters),
+                               # Pagination
+                               count=count,
+                               pager_url=pager_url,
+                               num_pages=num_pages,
+                               page=page,
+                               # Sorting
+                               sort_column=sort_idx,
+                               sort_desc=sort_desc,
+                               sort_url=sort_url,
+                               # Search
+                               search_supported=self._search_supported,
+                               clear_search_url=self._get_url('.index_view',
+                                                              None,
+                                                              sort_idx,
+                                                              sort_desc),
+                               search=search,
+                               # Filters
+                               filters=self._filters,
+                               filter_groups=self._filter_groups,
+                               filter_types=self._filter_types,
+                               filter_data=filters_data,
+                               active_filters=filters,
+
+                               # Actions
+                               actions=actions,
+                               actions_confirmation=actions_confirmation)
+
+    def get_pub_id(self):
+        try:
+            pub_id = int(login.current_user.pub_id)
+        except:
+            pub_id = None
+
+        return pub_id
