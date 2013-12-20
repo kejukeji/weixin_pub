a16126e7f6b97777c210d69b77cb11e54e8b9968
eXthen <exthen@gmail.com>
2013-12-20 15:56:08 +0800

    修复回复的bug

--- pub/weixin/message.py
+++ pub/weixin/message.py
@@ -4,11 +4,11 @@ import time
 
 TEXT = """
 <xml>
-<ToUserName>![CDATA[$ToUserName$]]</ToUserName>
-<FromUserName>![CDATA[$FromUserName$]]</FromUserName>
+<ToUserName><![CDATA[$ToUserName$]]></ToUserName>
+<FromUserName><![CDATA[$FromUserName$]]></FromUserName>
 <CreateTime>$CreateTime$</CreateTime>
-<MsgType>![CDATA[text]]</MsgType>
-<Content>![CDATA[$Content$]]</Content>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[$Content$]]></Content>
 </xml>
 """
 
